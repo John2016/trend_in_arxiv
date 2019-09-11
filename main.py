e3cf3644272d36065e9344e096bfc3165d29b485
Yuan <lhlxyjly@gmail.com>
2019-09-11 13:22:44 +0800

    start work

---
+++ main.py
@@ -0,0 +1,27 @@
+# utf-8
+
+## 2019/9/11
+
+import jieba
+import cloud
+
+
+field_list = ['CV','CL','PR']
+
+# get the total number of paper published in specific field on ARXIV
+# based on scrapy
+def get_num_paper_arxiv(field, month, year=2019)
+    if field not in field_list:
+        print('Invalid field name!')
+        return
+    
+    return
+
+# based on arxiv-api
+def get_num_paper_arxiv(field, month, year=2019)
+
+    return
+
+def generate_frequent_word(corpus, stopping_word = None):
+
+    return
